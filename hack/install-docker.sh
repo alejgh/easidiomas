32ccede042532dc4a3a2cc484b7fdfa454de8e8a
Guillermo Facundo (Willy) <10364506+thewillyhuman@users.noreply.github.com>
2021-02-19 17:51:37 +0100

    Add docker-swarm config scripts

---
+++ hack/install-docker.sh
@@ -0,0 +1,30 @@
+# Remove Old Docker Installations
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+# Install Docker
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+# Install Docker-Compose
+apt update -y
+apt upgrade -y
+apt install curl -y
+apt install docker -y
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
---
+++ hack/setup-master.sh
@@ -0,0 +1,2 @@
+docker swarm leave --force
+docker swarm init --advertise-addr 156.35.98.165
