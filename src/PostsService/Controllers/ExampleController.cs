68ac0779b73174dd3729db4aeabf2c77e1635493
Alejandro González Hevia <alejandrgh11@gmail.com>
2021-02-22 22:59:08 +0100

    Base app layout

---
+++ src/PostsService/Controllers/ExampleController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PostsService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExampleController : ControllerBase
+    {
+
+        // api/Example
+        [HttpGet]
+        public dynamic SayHello()
+        {
+            return new { Text = "Hello, World!" };
+        }
+    }
+}
