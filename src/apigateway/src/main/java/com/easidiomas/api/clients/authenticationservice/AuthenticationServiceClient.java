a36a3ff295f0a60a414fd0d1e5e1a6d6e3d6a1ed
Guillermo Facundo (Willy) <10364506+thewillyhuman@users.noreply.github.com>
2021-02-24 14:58:01 +0100

    Implementación inicial de la api gateway (#59)
    * Initialization of the apigateway
    
    * Add apigateway service to the deployment scripts
    
    * Fix last commit
    
    * sync to hot por GitHub
    
    * add names to containers and add the apigateway and autservice
    
    * comment slf4j implementation
    
    * fix the mechanism to get the env variables

---
+++ src/apigateway/src/main/java/com/easidiomas/api/clients/authenticationservice/AuthenticationServiceClient.java
@@ -0,0 +1,41 @@
+package com.easidiomas.api.clients.authenticationservice;
+
+import com.easidiomas.api.controllers.AuthenticationServiceController;
+import com.easidiomas.auth.AuthenticationServiceGrpc;
+import com.easidiomas.auth.Authservice;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AuthenticationServiceClient {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthenticationServiceController.class);
+
+    private final String serviceTarget = System.getenv("AUTH_SERVICE_ADDRESS")!=null ? System.getenv("AUTH_SERVICE_ADDRESS"): "loclhots:5000";
+    private final AuthenticationServiceGrpc.AuthenticationServiceBlockingStub authService;
+
+    public AuthenticationServiceClient() {
+        LOGGER.info(String.format("Connecting to authentication service on address [%s]", serviceTarget));
+        ManagedChannel channel = ManagedChannelBuilder.forTarget(serviceTarget)
+                .usePlaintext()
+                .build();
+        authService = AuthenticationServiceGrpc.newBlockingStub(channel);
+        LOGGER.info(String.format("Connected to authentication service on address [%s]", serviceTarget));
+    }
+
+    public Authservice.Token requestToken(String username, String password) {
+        Authservice.LoginInfo loginInfo = Authservice.LoginInfo.newBuilder()
+                .setUsername(username)
+                .setPassword(password)
+                .build();
+        return authService.generateToken(loginInfo);
+    }
+
+    public Authservice.Passport requestPassport(String stringToken) {
+        Authservice.Token token = Authservice.Token.newBuilder()
+                .setToken(stringToken)
+                .build();
+        return authService.generatePassport(token);
+    }
+}
