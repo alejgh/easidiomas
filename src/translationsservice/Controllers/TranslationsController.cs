a599cf8318192e80e3e04590c1bac25b9e417668
thewillyhuman <colunga91@gmail.com>
2021-02-24 16:44:33 +0100

    initial commit for the translations service

---
+++ src/translationsservice/Controllers/TranslationsController.cs
@@ -0,0 +1,46 @@
+﻿using Google.Cloud.Translation.V2;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+using translationsservice.Model;
+
+namespace translationsservice.Controllers
+{
+    [ApiController]
+    [Route("api/translations")]
+    public class TranslationsController : ControllerBase
+    {
+
+        private readonly ILogger<TranslationsController> _logger;
+
+        public TranslationsController(ILogger<TranslationsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public ActionResult<TranslationResponse> Translate([FromBody] TranslationRequest translationRequest)
+        {
+            _logger.LogInformation("Translation requested: " + translationRequest);
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            TranslationClient client = TranslationClient.Create();
+            
+            var response = client.TranslateText(
+                text: translationRequest.Text,
+                targetLanguage: translationRequest.TargetLanguage, 
+                sourceLanguage: translationRequest.SourceLanguage
+            );
+            var translation = response.TranslatedText;
+
+            _logger.LogInformation("Translation completed: " + translation);
+
+            TranslationResponse translationResponse = new TranslationResponse();
+            translationResponse.SourceLanguage = translationRequest.SourceLanguage;
+            translationResponse.TargetLanguage = translationRequest.TargetLanguage;
+            translationResponse.Text = translationRequest.Text;
+            translationResponse.Translation = translation;
+            return new CreatedResult("http://translationsservice/api/translations/" + DateTime.Now.GetHashCode(), translationResponse);
+        }
+    }
+}
---
+++ src/translationsservice/Model/TranslationRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace translationsservice.Model
+{
+    public class TranslationRequest
+    {
+        public string SourceLanguage { get; set; }
+        public string TargetLanguage { get; set; }
+        public string Text { get; set; }
+
+        public TranslationRequest() { }
+
+        public override string ToString()
+        {
+            return "TranslationRequest = SourceLaunguage [" + SourceLanguage + ", TargetLanguage ["
+                + TargetLanguage + ", Text [" + Text + "].";
+        }
+    }
+}
