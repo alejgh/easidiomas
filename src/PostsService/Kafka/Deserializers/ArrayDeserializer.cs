e174499646c94285c4fa09b499dcca7fff1f6beb
Alejandro González Hevia <alejandrgh11@gmail.com>
2021-02-25 21:08:57 +0100

    Implementación del servicio de posts (#61)
    * Base app layout
    
    * Logging to Kafka queue and initial startup ready
    
    * Implement kafka consumers
    
    * Document code and minor cleanups
    
    * Add posts service to docker-compose.yml
    
    * Add basic skeleton for REST API
    
    * Using Postgres as the database
    * Seed posts are created
    * Basic GET, POST, PUT and DELETE methods were scaffolded
    
    * Pagination, filtering and sorting setup
    
    * Refactor controller code and fix logging problems
    
    * Minor fixes to rest methods; added logging and autoincrement IDs
    
    * Integrate postgres with docker
    
    * Integrate topic modeling and posts services
    
    * Allow multiple languages as filter in GET posts
    
    * Allow multiple languages in query and documentation
    
    * Remove comment from json file
    
    * Rename posts service folder and postgres container

---
+++ src/PostsService/Kafka/Deserializers/ArrayDeserializer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Confluent.Kafka;
+
+namespace PostsService.Kafka.Deserializers
+{
+    /// <summary>
+    /// Custom deserializer to parse lists of strings received from kafka.
+    ///
+    /// The structure of the bytes array should be as follows: an integer
+    /// specifying the length of the next string; bytes representing the string;
+    /// if there are more strings in the array, another integer specifying
+    /// the length of the next string and so on...
+    /// </summary>
+    public class ArrayDeserializer : IDeserializer<string[]>
+    {
+
+        public string[] Deserialize(ReadOnlySpan<byte> data, bool isNull, SerializationContext context)
+        {
+            if (isNull) return null;
+
+            byte[] bytes = data.ToArray();
+            IList<string> res = new List<string>();
+            int i = 1;
+            while (i < bytes.Length)
+            {
+                int numBytesString = bytes[i];
+                i += 1;
+                string element = System.Text.Encoding.UTF8.GetString(bytes.Skip(i).Take(numBytesString).ToArray());
+                res.Add(element);
+                i += numBytesString;
+            }
+
+            return res.ToArray();
+        }
+    }
+}
--- src/topicmodelingservice/src/kafka/kafka_producer.py
+++ src/topicmodelingservice/src/kafka/kafka_producer.py
@@ -6,38 +6,32 @@ import sys
 from kafka import KafkaProducer
 from kafka.errors import NoBrokersAvailable
 
+from ..config import SERVICE_KEY
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+logger = logging.getLogger(SERVICE_KEY)
+logger.setLevel(logging.DEBUG)
 
 
-class KafkaLoggingHandler(logging.Handler):
+class Producer():
     """ Custom logging handler that redirects logs to a kafka queue
 
     Logs will be sent under the 'service_logs' topic.
     """
-    def __init__(self, endpoint, key="default", topic="service_logs"):
+    def __init__(self, endpoint):
         while not hasattr(self, 'producer'):
             try:
                 self.producer = KafkaProducer(bootstrap_servers=endpoint)
             except NoBrokersAvailable as nba:
                 logger.error("No broker was found. Retrying...")
                 time.sleep(3)
-        self.key = str.encode(key)
-        self.topic = topic
-        logging.Handler.__init__(self)
 
-    def emit(self, record):
-        if record.name == 'kafka':
-            return
+    def produce(self, topic, key, msg):
         try:
-            msg = self.format(record)
-            self.producer.send(self.topic, value=str.encode(msg), key=self.key)
+            logger.debug(f"Sending message to kafka queue[key={key}, msg={msg}]")
+            self.producer.send(topic, value=msg, key=key)
         except:
             ei = sys.exc_info()
+            print(ei)
             traceback.print_exception(ei[0], ei[1], ei[2], None, sys.stderr)
             del ei
 
-    def close(self):
-        self.producer.close()
-        logging.Handler.close(self)
