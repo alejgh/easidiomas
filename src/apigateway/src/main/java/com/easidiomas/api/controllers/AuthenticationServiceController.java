9b285475f1d7f2958e1fc736d7842f22e610e65f
thewilly <colunga91@gmail.com>
2021-02-28 00:12:26 +0100

    API gateway integrada con servicio de usuarios y de autenticación

--- src/apigateway/src/main/java/com/easidiomas/api/controllers/AuthenticationServiceController.java
+++ src/apigateway/src/main/java/com/easidiomas/api/controllers/AuthenticationServiceController.java
@@ -1,6 +1,7 @@
 package com.easidiomas.api.controllers;
 
 import com.easidiomas.api.clients.authenticationservice.AuthenticationServiceClient;
+import com.easidiomas.api.model.LoginInfo;
 import com.easidiomas.api.util.StringResponse;
 import com.easidiomas.auth.Authservice;
 import com.google.gson.Gson;
@@ -22,18 +23,22 @@ public class AuthenticationServiceController {
     private static final Logger LOGGER = LoggerFactory.getLogger(AuthenticationServiceController.class);
     private static final Gson GSON = new Gson();
 
-    @PostMapping(value = "token", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<StringResponse> requestToken(@RequestHeader("username") String username, @RequestHeader("password") String password) {
-        LOGGER.info(String.format("Request for token for username [%s] received", username));
-        LOGGER.info(String.format("Requesting token for username [%s]to the authentication service", username));
-        Authservice.Token token = null;
+    @PostMapping(value = "token", consumes= MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity requestToken(@RequestBody LoginInfo request) {
+        LOGGER.info(String.format("Request for token for username [%s] received", request.getUsername()));
+        LOGGER.info(String.format("Requesting token for username [%s] to the authentication service", request.getUsername()));
+
+        Authservice.TokenResponse token = null;
         try {
-            token = new AuthenticationServiceClient().requestToken(username, password);
+            token = new AuthenticationServiceClient().requestToken(request.getUsername(), request.getPassword());
         } catch (Exception e) {
-            LOGGER.warn(String.format("Generating token for user [%s] was not successful.", username));
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new StringResponse("Username or password were not correct."));
+            LOGGER.warn(String.format("Generating token for user [%s] was not successful.", request.getUsername()));
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
-        LOGGER.info(String.format("Token for username [%s] created", username));
-        return ResponseEntity.created(URI.create(SERVICE_URI+"/token/"+token.getToken())).body(new StringResponse(token.getToken()));
+        LOGGER.info(String.format("Token for username [%s] created", request.getUsername()));
+        return ResponseEntity
+                .created(URI.create(SERVICE_URI+"/token/"+token.getTokenGenerated()))
+                .body(new Gson().toJson(token));
     }
 }
--- src/apigateway/src/main/java/com/easidiomas/api/controllers/UsersServiceController.java
+++ src/apigateway/src/main/java/com/easidiomas/api/controllers/UsersServiceController.java
@@ -3,71 +3,84 @@ package com.easidiomas.api.controllers;
 import com.easidiomas.api.clients.usersservice.UserInfo;
 import com.easidiomas.api.filters.SecurityFilter;
 import com.easidiomas.auth.Authservice;
+import com.google.gson.Gson;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Nullable;
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Enumeration;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "api/users")
 public class UsersServiceController extends EasidiomasAPIController {
 
+    private static final String USERS_SERVICE_HOST = System.getenv("USERS_SERVICE_HOST")!=null ? System.getenv("USERS_SERVICE_HOST"): "localhost";
+    private static final int USERS_SERVICE_PORT = Integer.parseInt(System.getenv("USERS_SERVICE_PORT")!=null ? System.getenv("USERS_SERVICE_PORT"): "5000");
+
     public final static String SERVICE_URI = "https://www.easidiomas.com/api/users";
     private static final Logger LOGGER = LoggerFactory.getLogger(UsersServiceController.class);
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity index(HttpServletRequest request, @Nullable @RequestParam Integer limit,
-                                @Nullable @RequestParam Integer offset, @Nullable @RequestParam Integer minAge,
-                                @Nullable @RequestParam Integer maxAge, @Nullable @RequestParam String[] speaks,
-                                @Nullable @RequestParam String wantsToLearn) {
+    public ResponseEntity index(HttpServletRequest request) throws IOException, URISyntaxException {
         LOGGER.info(String.format("Request to path [%s] received.", request.getRequestURL()));
         LOGGER.info(String.format("Getting the passport from the header for request [%s].", request.getRequestURL()));
-        Authservice.Passport passport = super.getPassport(request);
+        //Authservice.Passport passport = super.getPassport(request);
 
         LOGGER.info(String.format("Requesting to the users service the list of users paginated [%s].", request.getRequestURL()));
-
-        // Propagar al servicio de clientes.
-        return ResponseEntity.ok().body("{\n" +
-                "\t“links”: {\n" +
-                "\t\t“self”: “url_actual”,\n" +
-                "\t\t“first”: “url_primeros_elementos”,\n" +
-                "\t\t“prev”: “url_pagina_anterior”,\n" +
-                "\t\t“next”: “url_pagina_siguiente”,\n" +
-                "\t\t“last”: “url_ultima_pagina”\n" +
-                "\t},\n" +
-                "\t“hits”: 10,\n" +
-                "\t“total”: 500,\n" +
-                "\t“users”: [\n" +
-                "\t{\n" +
-                "\t\t\"id\": 1,\n" +
-                "                \"name\": \"Pepito\",\n" +
-                "                \"surname\": \"Sanchez Perez\"\n" +
-                "\t\t\"learning\": [“en”, \"ru\"],\n" +
-                "                \"speaks\": \"es\",\n" +
-                "                \"birthDate\": \"<long_time_since_epoch>\",\n" +
-                "                \"avatar\": \"<url_del_avatar>\",\n" +
-                "                \"followers\": [\"<url_get_carmen>\", \"<url_get_paco>\"],\n" +
-                "                \"following\": [\"<url_get_fulanito>\", \"<url_get_menganita>\"]\n" +
-                "\t},\n" +
-                "\t…\n" +
-                "\t{\n" +
-                "\t\t\"id\": 10,\n" +
-                "\t\t…\n" +
-                "\t}\n" +
-                "\t]\n" +
-                "}");
+        LOGGER.info(String.format("Redirecting request to [%s:%s].", USERS_SERVICE_HOST, USERS_SERVICE_PORT));
+        return this.doRedirect(request, "/api/users");
     }
 
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity create(HttpServletRequest request, @RequestBody UserInfo user) {
+    public ResponseEntity create(HttpServletRequest request) throws IOException, URISyntaxException {
         LOGGER.info(String.format("Request to path [%s] received.", request.getRequestURL()));
         // Crear el usuario en el servicio de usuarios y actualizar el objeto de retorno de la response
         // Para que sea el objeto creado. Es decir, con el ID correcto.
-        return ResponseEntity.created(URI.create(SERVICE_URI+"/1")).body(user);
+        //return ResponseEntity.created(URI.create(SERVICE_URI+"/1")).body(user);
+        LOGGER.info(String.format("Redirecting request to [%s:%s].", USERS_SERVICE_HOST, USERS_SERVICE_PORT));
+        return this.doRedirect(request, "/api/users");
+    }
+
+    private ResponseEntity<String> doRedirect(HttpServletRequest request, String path) throws URISyntaxException, IOException {
+        // obtener uri a la que enviar la nueva peticion
+        URI thirdPartyApi = new URI("http", null, USERS_SERVICE_HOST, USERS_SERVICE_PORT, path, request.getQueryString(), null);
+        Authservice.Passport passport = super.getPassport(request);
+
+        // copiar headers a la nueva peticion
+        HttpHeaders headers = new HttpHeaders();
+        for (Enumeration<?> e = request.getHeaderNames(); e.hasMoreElements();) {
+            String nextHeaderName = (String) e.nextElement();
+            String headerValue = request.getHeader(nextHeaderName);
+            headers.add(nextHeaderName, headerValue);
+        }
+
+        // esto no se muy bien como va. creo que ya debería estar el passport aqui asi que seguramente se pueda borrar
+        if (passport != null) headers.add("passport", new Gson().toJson(passport));
+
+        // obtener el body del post
+        String body = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+
+        // enviamos la peticion a la nueva uri
+        RestTemplate restTemplate = new RestTemplate();
+        HttpEntity<String> entity = new HttpEntity<String>(body, headers);
+
+        ResponseEntity<String> resp;
+        try {
+            resp =
+                    restTemplate.exchange(thirdPartyApi, HttpMethod.resolve(request.getMethod()), entity, String.class);
+        } catch (HttpClientErrorException e) {
+            resp =
+                    ResponseEntity.status(e.getStatusCode()).body(e.getMessage());
+        }
+        return resp;
     }
 }
