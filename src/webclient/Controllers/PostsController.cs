ee002dff7924ccc5cdbaf0e1f5dc2c19255b977f
Alejandro González Hevia <alejandrgh11@gmail.com>
2021-02-27 17:25:03 +0100

    Cambios en API Gateway y login funcionando en web client (#67)
    * Initial project scaffolding and .gitignore
    
    * Return json from auth endpoint
    
    * Base app layout for web client and login working

---
+++ src/webclient/Controllers/PostsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace WebClient.Controllers
+{
+    public class PostsController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index(bool isOffensive=false)
+        {
+            return View();
+        }
+    }
+}
