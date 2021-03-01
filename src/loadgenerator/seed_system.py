35e7907dbc515d3095c73d7495fdbbe6ed17ef82
Alejandro González Hevia <alejandrgh11@gmail.com>
2021-03-01 14:55:51 +0100

    Loop

--- src/loadgenerator/seed_system.py
+++ src/loadgenerator/seed_system.py
@@ -182,3 +182,14 @@ for msg, chat_idx, author_idx in zip(messages, messages_chat, messages_author):
         json=msg, headers=headers, verify=False)
     logger.debug(f"Response code: {r_msg.status_code}")
     logger.debug(f"Response: {r_msg.text}")
+
+
+while True:
+    logger.debug("Doing get to users")
+    token = do_login(users[0])
+    headers = {'token': token}
+    r = requests.get(API_GATEWAY_ENDPOINT + "/users", headers=headers, verify=False)
+    logger.debug(f"Response code: {r.status_code}")
+    logger.debug(f"Response: {r.text}")
+    logger.debug(f"Sleeping")
+    time.sleep(600)
